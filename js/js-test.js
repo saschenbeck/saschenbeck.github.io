8841f679bccd6c481e7511da9469c5571c5fd923
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-12 11:03:51 -0500

    added colorButton var to js; added rainbow array; added color changing function

--- js/js-test.js
+++ js/js-test.js
@@ -1,3 +1,10 @@
 "use strict";
 
 console.log("This is a JS Test");
+
+var colorButton = document.getElementsByClassName('colorChange');
+var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
+
+colorButton.onclick = function () {
+    this.style.backgroundColor = 'blue'
+};
