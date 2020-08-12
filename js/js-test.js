549b9798f08eb28adf2a70223a1721acec0b36a4
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-12 12:13:41 -0500

    created change function to store color change; added onclick functionality to button on html

--- js/js-test.js
+++ js/js-test.js
@@ -7,8 +7,12 @@ var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
 var randomColor = rainbow[Math.floor(Math.random()*rainbow.length)];
 
 
-colorButton.onclick = function () {
-    this.style.backgroundColor = randomColor;
-};
+// colorButton.onclick = function () {
+//     this.style.backgroundColor = randomColor;
+// };
+
+function change (){
+    colorButton.style.backgroundColor = randomColor;
+}
 
 console.log(randomColor)
