8aca23fe6fe96dec419d2b96107155bd427e4ecc
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-12 11:57:49 -0500

    Added a random color var and applied it to onclick function

--- js/js-test.js
+++ js/js-test.js
@@ -3,9 +3,12 @@
 console.log("This is a JS Test");
 
 var colorButton = document.getElementById('testButton');
-// var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
+var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
+var randomColor = rainbow[Math.floor(Math.random()*rainbow.length)];
 
 
 colorButton.onclick = function () {
-    this.style.backgroundColor = 'blue'
+    this.style.backgroundColor = randomColor;
 };
+
+console.log(randomColor)
