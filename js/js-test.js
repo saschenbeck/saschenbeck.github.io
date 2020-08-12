f742b99409e21ed2769a703277d46948fd896dbd
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-12 11:37:47 -0500

    No major changes

--- js/js-test.js
+++ js/js-test.js
@@ -5,6 +5,7 @@ console.log("This is a JS Test");
 var colorButton = document.getElementsByClassName('colorChange');
 var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
 
+
 colorButton.onclick = function () {
     this.style.backgroundColor = 'blue'
 };
