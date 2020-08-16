4017ec768a16c2f9bee90be6f091e5f963da2af2
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-15 21:42:01 -0500

    made rainbow and random color local variables in the change function to enable new colors upon button click

--- js/js-test.js
+++ js/js-test.js
@@ -3,8 +3,7 @@
 console.log("This is a JS Test");
 
 var colorButton = document.getElementById('testButton');
-var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
-var randomColor = rainbow[Math.floor(Math.random()*rainbow.length)];
+
 
 
 // colorButton.onclick = function () {
@@ -12,8 +11,11 @@ var randomColor = rainbow[Math.floor(Math.random()*rainbow.length)];
 // };
 
 function change (){
+    var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
+    var randomColor = rainbow[Math.floor(Math.random()*rainbow.length)];
     colorButton.style.backgroundColor = randomColor;
     var newColor = randomColor
     console.log(newColor);
 }
+
 
