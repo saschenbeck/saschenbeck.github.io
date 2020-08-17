37fb3fc00df7e87f1343d4da52130e6fbd2c42c4
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-17 17:42:00 -0500

    provided color section with flex display, applied random color to section

--- js/js-test.js
+++ js/js-test.js
@@ -3,19 +3,19 @@
 console.log("This is a JS Test");
 
 var colorButton = document.getElementById('testButton');
+var colorSection = document.getElementById('colorSection');
 
 
-
-// colorButton.onclick = function () {
-//     this.style.backgroundColor = randomColor;
-// };
 
 function change (){
     var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
-    var randomColor = rainbow[Math.floor(Math.random()*rainbow.length)];
-    colorButton.style.backgroundColor = randomColor;
-    var newColor = randomColor
-    console.log(newColor);
+    var randomButtonColor = rainbow[Math.floor(Math.random()*rainbow.length)];
+    var randomSectionColor = rainbow[Math.floor(Math.random()*rainbow.length)];
+    colorButton.style.backgroundColor = randomButtonColor;
+    var newButtonColor = randomButtonColor
+    colorSection.style.backgroundColor = randomSectionColor;
+    var newSectionColor = randomButtonColor
+    console.log(newButtonColor,newSectionColor);
 }
 
 
