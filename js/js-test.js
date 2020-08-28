bedf1c6258e16ce85a16f524f8e7ca2aa922b77b
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-28 11:50:10 -0500

    feat: moved change function into testButton.onclick

--- js/js-test.js
+++ js/js-test.js
@@ -6,8 +6,17 @@ console.log("This is a JS Test");
 
 
 
-function change (){
-    var colorButton = document.getElementById('testButton');
+// function change (){
+//     var colorButton = document.getElementById('testButton');
+//     var colorSection = document.getElementById('colorSection');
+//     var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
+//     var randomButtonColor = rainbow[Math.floor(Math.random()*rainbow.length)];
+//     var randomSectionColor = rainbow[Math.floor(Math.random()*rainbow.length)];
+//     colorButton.style.backgroundColor = randomButtonColor;
+//     colorSection.style.backgroundColor = randomSectionColor;
+// }
+
+testButton.onclick = function () {var colorButton = document.getElementById('testButton');
     var colorSection = document.getElementById('colorSection');
     var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
     var randomButtonColor = rainbow[Math.floor(Math.random()*rainbow.length)];
