ff2f476f85dcb80cf29b45441ce83090e9721734
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-08-28 09:15:01 -0500

    feat: removed absolute positioning from footer, isolated variables for color change button to the function change()

--- js/js-test.js
+++ js/js-test.js
@@ -2,20 +2,18 @@
 
 console.log("This is a JS Test");
 
-var colorButton = document.getElementById('testButton');
-var colorSection = document.getElementById('colorSection');
+
 
 
 
 function change (){
+    var colorButton = document.getElementById('testButton');
+    var colorSection = document.getElementById('colorSection');
     var rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
     var randomButtonColor = rainbow[Math.floor(Math.random()*rainbow.length)];
     var randomSectionColor = rainbow[Math.floor(Math.random()*rainbow.length)];
     colorButton.style.backgroundColor = randomButtonColor;
-    var newButtonColor = randomButtonColor
     colorSection.style.backgroundColor = randomSectionColor;
-    var newSectionColor = randomButtonColor
-    console.log(newButtonColor,newSectionColor);
 }
 
 
